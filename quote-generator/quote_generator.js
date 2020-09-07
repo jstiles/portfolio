eb1ee1f072a04fa623ee3de56781fac10ebd6571
jim <jstiles@ieee.org>
2020-09-07 13:56:56 -0600

    add own aqueous-sierra cors anywhere server

--- quote-generator/quote_generator.js
+++ quote-generator/quote_generator.js
@@ -23,7 +23,8 @@ function complete() {
 async function getQuote() {
   loading();
   // replace proxyUrl  TBD
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+  //const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+  const proxyUrl = 'https://aqueous-sierra-03944.herokuapp.com/'
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
     const response = await fetch(proxyUrl + apiUrl);
